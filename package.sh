68111b49b2953c1d05706537163c371257ffe326
Chi_Tang <me@chitang.dev>
2025-11-02 16:22:02 +0800

    script: try fix

--- package.sh
+++ package.sh
@@ -3,16 +3,16 @@
 rm beszel_agent-for-magisk.zip 2>/dev/null
 # rm -rf beszel
 git clone --depth=1 https://github.com/henrygd/beszel.git
-pushd beszel/beszel
+pushd beszel
 go mod tidy
 mkdir -p output
-GOOS=linux GOARCH=arm64 go build -v -o output/arm64 -ldflags "-w -s" beszel/cmd/agent
-GOOS=linux GOARCH=arm go build -v -o output/arm -ldflags "-w -s" beszel/cmd/agent
-GOOS=linux GOARCH=amd64 go build -v -o output/x64 -ldflags "-w -s" beszel/cmd/agent
+GOOS=linux GOARCH=arm64 go build -v -o output/arm64 -ldflags "-w -s" ./internal/cmd/agent
+GOOS=linux GOARCH=arm go build -v -o output/arm -ldflags "-w -s" ./internal/cmd/agent
+GOOS=linux GOARCH=amd64 go build -v -o output/x64 -ldflags "-w -s" ./internal/cmd/agent
 popd
 
 mkdir -p module/beszel/bin
-mv beszel/beszel/output/* module/beszel/bin/
+mv beszel/output/* module/beszel/bin/
 
 beszel_version=$(module/beszel/bin/x64 version | sed 's/beszel-agent //')
 sed -i "s/AGENT_VERSION/${beszel_version}/g" module/module.prop
